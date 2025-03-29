8854fae21f345082961890475224ca1976d6ac59
Pradeep Sharma <kpradeepsharma@hotmail.com>
2025-03-29 16:12:37 +0530

    Time: 0 ms (100.00%) | Memory: 49.8 MB (35.07%) - LeetSync

---
+++ 169-majority-element/majority-element.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public int MajorityElement(int[] arr) {
+        int major =0; // Considering this as the major element in the array
+		int freq=0; // initialize the frequency with zero
+		
+		// Voting algo where is is sure that at least one element would always be there in the array.
+		for (int i=0; i< arr.Length; i++){
+			
+			if (freq == 0)
+				major = arr[i];
+			if (major == arr[i]) freq++;
+			else freq--;
+		}
+		
+		return major;
+    }
+}
